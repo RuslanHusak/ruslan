03d55645e430dee35d2f58339a8b5861124517d2
RuslanHusak <94846544+RuslanHusak@users.noreply.github.com>
2021-12-20 22:24:19 +0200

    Add files via upload

---
+++ #1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace еліпс
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            if (x*x/4+y*y/1<1)||(x>1)&&(x<-1)
+            Console.WriteLine("YES");
+            else
+            Console.WriteLine("NO");
+            
+        }
+    }
+}
